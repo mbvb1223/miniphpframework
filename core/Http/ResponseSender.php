871b28c1d9b967c5ddb3874d22de519c0062adb3
Khien <kcp@cct-technology.com>
2025-07-17 08:07:20 +0700

    .

--- core/Http/ResponseSender.php
+++ core/Http/ResponseSender.php
@@ -2,12 +2,28 @@
 
 namespace Khien\Http;
 
-use GuzzleHttp\Psr7\Response;
-
 class ResponseSender
 {
     public function send(Response $response): Response
     {
+        ob_start();
+
+        $this->sendContent($response);
+
+        ob_end_flush();
+
         return $response;
+    }
+
+    public function sendContent(Response $response)
+    {
+        $body = $response->body;
+        if (is_array($body)) {
+            echo json_encode($body);
+        } else {
+            echo $body;
+        }
+
+        ob_flush();
     }
 }
